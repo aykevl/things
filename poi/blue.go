4581ad01318fc09c2d1e10666ee20a7394c799cf
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-04-16 01:20:34 +0200

    poi: fix default brightness of blue poi

--- poi/blue.go
+++ poi/blue.go
@@ -6,6 +6,6 @@ import "image/color"
 
 // Appearance configuration.
 var (
-	baseColor     = color.RGBA{0, 0, 0xff, 0x11}
+	baseColor     = color.RGBA{0, 0, 0xff, 0x08}
 	bluetoothName = "blue poi"
 )
