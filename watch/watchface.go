30b0f6e75b6d7166c35155912193081ddfedbf4c
Ayke van Laethem <aykevanlaethem@gmail.com>
2023-11-21 19:59:55 +0100

    watch: fix watchface after adjusting time
    There was a bug where the watch would show the old time after adjusting
    only the hours (but not the minutes). This is fixed by also checking
    whether the hours have been updated.

--- watch/watchface.go
+++ watch/watchface.go
@@ -15,7 +15,10 @@ func (w *Watch[T]) createWatchFace(views *ViewManager[T]) View[T] {
 		white = pixel.NewColor[T](255, 255, 255)
 	)
 
-	timeText := tinygl.NewText(&freesans.Regular24pt7b, white, black, "00:00")
+	now := watchTime()
+	hour := now.Hour()
+	minute := now.Minute()
+	timeText := tinygl.NewText(&freesans.Regular24pt7b, white, black, formatTime(hour, minute))
 	eventWrapper := tinygl.NewEventBox[T](timeText)
 	eventWrapper.SetEventHandler(func(event tinygl.Event, x, y int) {
 		if event == tinygl.TouchTap {
@@ -32,15 +35,16 @@ func (w *Watch[T]) createWatchFace(views *ViewManager[T]) View[T] {
 		}
 	})
 
-	var minute int = -1
 	return NewView[T](eventWrapper, func(now time.Time) {
 		// Update the watchface.
 		if backlight > 0 {
 			// Watch face is visible.
+			newHour := now.Hour()
 			newMinute := now.Minute()
-			if minute != newMinute {
+			if hour != newHour || minute != newMinute {
+				hour = newHour
 				minute = newMinute
-				timeText.SetText(formatTime(now.Hour(), minute))
+				timeText.SetText(formatTime(hour, minute))
 			}
 		}
 	})
